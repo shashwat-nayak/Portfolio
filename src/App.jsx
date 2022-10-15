56cca4818dba184e93b995ab07a6cbe2be5427b3
HarshRajGupta <HarshRajGupta@hotmail.com>
2022-10-15 16:33:31 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import { memo } from 'react';
 import Styled from 'styled-components';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './Style.scss';
 import NavBar from './components/NavBar';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Container id={'app'}>
-				<NavBar />
-				<Routes>
-					<Route
-						path={'/*'}
-						element={
-							<img
-								src="/Logo.svg"
-								alt="Logo"
-							/>
-						}
-					/>
-				</Routes>
-			</Container>
-		</BrowserRouter>
+		<Container id={'app'}>
+			<NavBar />
+		</Container>
 	);
 }
 
